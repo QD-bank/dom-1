5523527aac3ce1fafaac40e663171a7326c4a6a0
jackli <lijunll35@163.com>
2021-08-07 22:09:28 +0800

    提交

---
+++ src/main.js
@@ -0,0 +1,57 @@
+//增
+const div = dom.create("<div>newDiv</div>");
+console.log(div);
+
+dom.after(test, div);
+
+const div3 = dom.create('<div id="parent"></div>')
+dom.wrap(test, div3)
+
+//删
+const nodes = dom.empty(window.empty)
+console.log(nodes)
+
+//改
+
+dom.attr(test, 'title', 'Hi, I am Tom')
+const title  = dom.attr(test, 'title')
+console.log(`title: ${title}`)
+
+dom.text(test, '你好，这是新的内容')
+dom.text(test)
+
+dom.style(test, {border: '1px solid pink', color: 'yellow'})
+console.log(dom.style(test, 'border'))
+dom.style(test, 'border', '1px solid pink')
+
+//添加类
+dom.class.add(test, 'red')
+dom.class.add(test, 'black')
+//删除类
+dom.class.remove(test, 'pink')
+
+//事件————添加—————移除
+const fn = () =>{
+    console.log('点击了')
+}
+dom.on(test, 'click', fn)
+dom.off(test, 'click', fn)
+
+//查
+
+const testDiv = dom.find('#test')[0]
+console.log(testDiv)
+const test2 = dom.find('#test2')[0]
+console.log(dom.find('.red', test2)[0])
+
+console.log(dom.parent(test))
+
+const s2 = dom.find('#s2')[0]
+console.log(dom.siblings(s2))
+console.log(dom.next(s2))
+console.log(dom.previous(s2))
+
+const t = dom.find('#travel')[0]
+dom.each(dom.children(t), (n)=> dom.style(n, 'color', 'red'))
+
+console.log(dom.index(s2))
